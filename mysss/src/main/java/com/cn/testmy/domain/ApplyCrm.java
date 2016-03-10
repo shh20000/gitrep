3fa37ad33dc0980cf51aca432449abf91e5ef521
shh20000 <shh20000@163.com>
2016-03-10 17:15:52 +0800

    Update ApplyCrm.java

--- mysss/src/main/java/com/cn/testmy/domain/ApplyCrm.java
+++ mysss/src/main/java/com/cn/testmy/domain/ApplyCrm.java
@@ -2,9 +2,7 @@ package com.cn.testmy.domain;
 
 import java.io.Serializable;
 import java.util.Date;
-/*
- * Author: Simon Shu
- */
+
 public class ApplyCrm implements Serializable {
 	private static final long serialVersionUID = -4669999477823766186L;
 	
